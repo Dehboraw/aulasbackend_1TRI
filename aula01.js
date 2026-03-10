00b16429439742c8ee66af8677e924f28db22b3a
Dehboraw <alvezdebora2007@gmail.com>
2026-03-10 09:23:12 -0300

    Add files via upload

---
+++ aula01.js
@@ -0,0 +1,10 @@
+// Tipagem de dados no JS
+// Tipagem dinâmica: o próprio interpretador atribui o tipo do dado da variável
+nome = "Débora"
+idade = 18
+altura = 1.60
+estuda = true
+console.log(typeof(nome))
+console.log(typeof(idade))
+console.log(typeof(altura))
+console.log(typeof(estuda))
